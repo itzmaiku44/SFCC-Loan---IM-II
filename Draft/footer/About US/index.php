f942110445c38ac7f45408952d7216c934f1c24e
Geirmane <blackcrusher44@gmail.com>
2024-07-03 14:24:49 +0800

    Add files via upload

---
+++ Draft/footer/About US/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SFCC ABOUT US</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="footer" style = "background-color : #534fc2;" >
+    <div class="container">
+        <div class="column">
+            <h3>ABOUT - SFCC</h3>
+            <ul>
+                <li><a href="#">Who we are</a></li>
+                <li><a href="#">Investor Relations</a></li>
+                <li><a href="#">Governance</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Sustainability</a></li>
+                <li><a href="#">Careers</a></li>
+            </ul>
+        </div>
+        <div class="column">
+            <h3>SITE LINKS</h3>
+            <ul>
+                <li><a href="https://privacy.gov.ph/data-privacy-act/">Data Privacy</a></li>
+                <li><a href="https://www.bsp.gov.ph/Inclusive%20Finance/Modules/Module%206%20Consumer%20Protection.pdf">Financial Consumer Protection</a></li>
+            </ul>
+        </div>
+        <div class="column">
+            <h3>OFFICE</h3>
+            <address>
+            P. del Rosario St., Cebu City,<br> 6000, Philippines<br>
+            </address>
+            <div class="social-icons">
+                <br>
+                <a href="https://www.facebook.com"><img src="Picturs/fb.svg" alt="Facebook"></a> 
+                <a href="https://www.instagram.com"><img src="Picturs/ig.svg" alt="Instagram"></a>
+                <a href="https://www.youtube.com"><img src="Picturs/youtube.svg" alt="YouTube"></a>
+                <a href="https://www.x.com"><img src="Picturs/twitter.svg" alt="Twitter"></a>
+                <a href="https://www.linkedin.com"><img src="Picturs/linkedin.svg" alt="LinkedIn"></a>
+            </div>
+
+        </div>
+    </div>
+    <div class="footer-bottom" style = "background-color : #7d7ad6;">
+        <div class="container">
+            <p><font color="black">SFCC is a proud member of <strong>BancNet</strong> | SFCC is regulated by the Bangko Sentral ng Pilipinas <a href="https://www.bsp.gov.ph">https://www.bsp.gov.ph</a></p>
+            <p>© Copyright 2024. All rights reserved.</p></font>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
